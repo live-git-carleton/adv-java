4aaf57ba528c0e48c59d5d77b9846c6b8054cf3b
craft-code-dec <crafted.coded.2003@gmail.com>
2020-06-19 03:39:03 +0530

    .

--- jl-adv-java-master/src/cloning/FooDeepClone.java
+++ jl-adv-java-master/src/cloning/FooDeepClone.java
@@ -1,19 +1,20 @@
+package cloning;
 
 import java.util.Arrays;
 
-public class FooShallowClone implements Cloneable {
+public class FooDeepClone implements Cloneable {
 	public int[] nums;
 	public String nameString = "Foo";
 	public int age = 100;
 
-	public FooShallowClone(int size) {
+	public FooDeepClone(int size) {
 		nums = new int[size];
 	}
 
 	public static void main(String[] args) throws CloneNotSupportedException {
-		FooShallowClone f1 = new FooShallowClone(5);
+		FooDeepClone f1 = new FooDeepClone(5);
 		f1.nums[0] = 100;
-		FooShallowClone f2 = (FooShallowClone) f1.clone();
+		FooDeepClone f2 = (FooDeepClone) f1.clone();
 		System.out.println("fl" + f1);
 		System.out.println("f2" + f2);
 		f2.nameString = "Bar";
@@ -21,10 +22,23 @@ public class FooShallowClone implements Cloneable {
 		f2.nums[2] = 2000;
 		System.out.println("fl" + f1);
 		System.out.println("f2" + f2);
-		f1.nums[1] = 700;
-		f2.age = 110;
-		System.out.println("f2" + f2);
-		System.out.println("fl" + f1);
+//		f1.nums[1] = 700;
+//		f2.age = 110;
+//		System.out.println("f2" + f2);
+//		System.out.println("fl" + f1);
+
+	}
+
+	@Override
+	protected Object clone() {
+		FooDeepClone fooDeepClone;
+		try {
+			fooDeepClone = (FooDeepClone) super.clone();
+			fooDeepClone.nums = (int[]) nums.clone();
+			return fooDeepClone;
+		} catch (Exception e) {
+			return null;
+		}
 
 	}
 
