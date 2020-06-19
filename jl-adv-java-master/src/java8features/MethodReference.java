91cee813976969d931ddf62ea8910228d30f89fd
craft-code-dec <crafted.coded.2003@gmail.com>
2020-06-20 03:33:02 +0530

    .

--- jl-adv-java-master/src/java8features/MethodReference.java
+++ jl-adv-java-master/src/java8features/MethodReference.java
@@ -6,9 +6,11 @@ import java.util.List;
 public class MethodReference {
 	public static void main(String[] args) {
 		List<String> namesList = new ArrayList<String>();
-		namesList.add("Anthony Robbins");
+		namesList.add("Anthony");
 		namesList.add("Colette");
-		namesList.forEach((element) -> System.out.println(element));
 
+		// namesList.forEach((element) -> System.out.println(element));
+
+		namesList.forEach(System.out::println);
 	}
 }
